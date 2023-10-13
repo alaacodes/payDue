2761fd385cb4c09b47cd39850c6cfe93dced3ad4
kinglemar <dimeji.ogunleye20@gmail.com>
2023-10-13 08:39:18 +0100

    Add: Development Enviroment.

--- src/app.js
+++ src/app.js
@@ -53,7 +53,6 @@ if (config.env === 'production') {
 // v1 api routes
 app.use('/v1', routes);
 
-// send back a 404 error for any unknown api request
 app.use((req, res, next) => {
   next(new ApiError(httpStatus.NOT_FOUND, 'Not found'));
 });
